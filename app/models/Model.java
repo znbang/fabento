3352faf8ae629ee56c8e2252d7a50faaaf5850e2
Zhenbang Wei <forth@fineart-tech.com>
2012-01-17 15:13:22 +0800

    JPASupport is deprecated, use GenericModel.

--- app/models/Model.java
+++ app/models/Model.java
@@ -5,15 +5,20 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
-import play.db.jpa.JPASupport;
+import play.db.jpa.GenericModel;
 
 @MappedSuperclass
-public class Model extends JPASupport {
+public class Model extends GenericModel {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     public Long id;
 
     public Long getId() {
         return id;
+    }
+
+    @Override
+    public Object _key() {
+        return getId();
     }
 }
